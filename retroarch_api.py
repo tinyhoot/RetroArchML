4ed044fc550efab724be70807381ef4ac118bbf5
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2022-02-21 18:44:03 +0100

    Remove stdin communication for good.

--- retroarch_api.py
+++ retroarch_api.py
@@ -1,7 +1,6 @@
 import logging
 import socket
 import subprocess
-import threading
 import time
 from subprocess import Popen
 from typing import Union, Tuple
@@ -13,30 +12,20 @@ class RetroArchAPI:
     _ip = "localhost"
     _port = 55355
 
-    def __init__(self, retroarch: str, core: str, rom: str, enable_stdin: bool = False):
-        """Run a RetroArch process and prepare it for network or stdin communication.
+    def __init__(self, retroarch: str, core: str, rom: str):
+        """Run a RetroArch process and prepare it for network communication.
 
         :param retroarch: The path to a retroarch executable.
         :param core: The path to the libretro core used for running the game.
         :param rom: The path to the rom of the game to be run.
-        :param enable_stdin: If enabled, will try to use stdin rather than network commands to control RetroArch.
         """
         # Init the RetroArch process
         self._process = Popen([retroarch, "-L", core, rom, "--appendconfig", "config.cfg", "--verbose"], bufsize=1,
                               stdin=subprocess.PIPE, stdout=None, stderr=None, text=True)
 
-        # Init the stdout monitor thread
-        if enable_stdin:
-            self._stdout_queue = []
-            self._stdout_event = threading.Event()
-            self._stdout_lock = threading.Lock()
-            stdout_monitor = threading.Thread(target=self._monitor_stdout, daemon=True)
-            stdout_monitor.start()
-
         # Init the network socket
-        if not enable_stdin:
-            self._socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-            self._socket.settimeout(3)
+        self._socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self._socket.settimeout(3)
 
         logging.info("Finished initialisation.")
 
@@ -58,53 +47,6 @@ class RetroArchAPI:
         # Filter out the input command
         index = response.find(" ")
         return response[:index], response[index+1:]
-
-    def _monitor_stdout(self):
-        """Monitor RetroArch's stdout and stderr pipelines. Intended to be run as a separate thread.
-
-        For some reason, RetroArch seems to log everything to stderr. This is redirected to stdout during
-        process initialisation and also captured here.
-        """
-        logging.debug("Starting threaded stdout monitor.")
-
-        while self._process.poll() is None:
-            self._stdout_event.clear()
-            # If stdout is currently empty, this will hang until there is something to be read.
-            line = self._process.stdout.readline().rstrip()
-            # Ensure the queue object is consistent across threads.
-            with self._stdout_lock:
-                self._stdout_queue.append(line)
-                # Notify the main thread that a change has occurred.
-                self._stdout_event.set()
-            logging.debug("STDOUT: " + line)
-
-        logging.debug("Process has ended, stopping stdout monitor thread.")
-
-    def _flush_stdout(self):
-        """Shorten the stdout queue by discarding old entries."""
-        if len(self._stdout_queue) > 10:
-            with self._stdout_lock:
-                self._stdout_queue = self._stdout_queue[-10:]
-
-    def _read_stdout(self) -> Union[str, None]:
-        """Read the most recent addition to the stdout queue."""
-        with self._stdout_lock:
-            # If the queue is empty, just return None instead.
-            if len(self._stdout_queue) == 0:
-                return None
-            last_line = self._stdout_queue.pop()
-        # Calling this here will probably keep the stdout queue reasonably short.
-        self._flush_stdout()
-
-        return last_line
-
-    def _write_stdin(self, command: str):
-        """Send a command to RetroArch via stdin."""
-        # Ensure the command is properly formatted
-        command = command.rstrip() + "\n"
-
-        logging.info("Writing command to stdin: "+command.rstrip())
-        self._process.stdin.write(command)
 
     def _get_network_response(self, bufsize: int) -> bytes:
         """Receive a network response from RetroArch.
@@ -204,5 +146,3 @@ class RetroArchAPI:
         b_arr = bytearray.fromhex(response_str[index+1:])
 
         return b_arr
-
-
