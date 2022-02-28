31233c1ff323ceb9c2e148c624520cc13915926b
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2022-02-28 21:35:11 +0100

    Implement breeding, compatibility distance, and fitness functions.

--- models/neatevolve.py
+++ models/neatevolve.py
@@ -1,21 +1,41 @@
-import logging
-from typing import Tuple, Union
+"""An implementation of the NEATevolve algorithm, as described in:
+Stanley, K. O., & Miikkulainen, R. (2002). Evolving neural networks through augmenting topologies. Evolutionary
+computation, 10(2), 99-127. https://doi.org/10.1162/106365602320169811
+"""
 
-_log = logging.getLogger(__name__)
+import logging
+import math
+from random import Random
+from typing import Tuple, Union, List, Sequence
+
+LOG = logging.getLogger(__name__)
+RANDOM = Random()
+
+EXCESS_GENES_COEFFICIENT = 1.0
+DISJOINT_GENES_COEFFICIENT = 1.0
+WEIGHT_DIFFERENCE_COEFFICIENT = 0.4
+DISABLED_GENE_REENABLE_CHANCE = 0.25
 
 
 class Connection:
     """A connection gene between two node genes."""
 
-    def __init__(self, input: int, output: int, weight: float, innovation: int):
-        self.input_node = input
-        self.output_node = output
+    def __init__(self, input_node: int, output_node: int, innovation: int, weight: float = 0.5):
+        self.input_node = input_node
+        self.output_node = output_node
+        self.innovation = innovation
         self.weight = weight
         self.enabled = True
-        self.innovation = innovation
+
+    def copy(self) -> "Connection":
+        """Get a full copy of this gene."""
+        duplicate = Connection(self.input_node, self.output_node, self.innovation, self.weight)
+        duplicate.enabled = self.enabled
+        return duplicate
 
 
 class Node:
+    """A node gene, connected to other nodes by connection genes."""
 
     def __init__(self, node_id: int):
         self.id = node_id
@@ -26,57 +46,62 @@ class Genome:
     """A collection of node genes connected by connection genes."""
 
     def __init__(self):
-        self.input_nodes = []
-        self.hidden_nodes = []
-        self.output_nodes = []
-        self.connections = []
+        self.input_nodes: List[Node] = []
+        self.hidden_nodes: List[Node] = []
+        self.output_nodes: List[Node] = []
+        self.connections: List[Connection] = []
+        self.fitness = 0
 
-    def get_connection(self, input_node: int = None, output_node: int = None, innovation: int = None) -> Connection:
+    def get_connection(self, input_node: int = None, output_node: int = None, innovation: int = None) \
+            -> Union[Connection, None]:
         """Get the connection gene with either the specified innovation number or the in- and output nodes.
 
         :param innovation: The innovation number to look for.
         :param input_node: The id of an input node to look for. Only usable with output_node.
         :param output_node: The id of an output node to look for. Only usable with input_node.
-        :return: A connection gene.
-        :raise ValueError: If no connection matches the given parameters.
+        :return: A connection gene, or None if the connection does not exist.
+        :raise ValueError: If the given parameters are faulty or incomplete.
         """
-        _log.debug(f"Finding connection in:{input_node}, out:{output_node}, innovation:{innovation}")
+        LOG.debug(f"Finding connection in:{input_node}, out:{output_node}, innovation:{innovation}")
         if innovation is not None:
             for connection in self.connections:
                 if connection.innovation == innovation:
                     return connection
             # No such connection exists.
             # _log.warning(f"No such connection with innovation {innovation}")
-            raise ValueError(f"There is no connection gene with the innovation number {innovation}!")
+            # raise ValueError(f"There is no connection gene with the innovation number {innovation}!")
+            return None
 
         if input_node is not None and output_node is not None:
             for connection in self.connections:
                 if connection.input_node == input_node and connection.output_node == output_node:
                     return connection
             # _log.warning(f"No such connection with in:{input_node}, out:{output_node}")
-            raise ValueError(f"There is no connection gene with input node {input_node} and output node {output_node}!")
+            # raise ValueError(f"There is no connection gene with input node {input_node} and output node {output_node}!")
+            return None
 
-        # None of the input parameters yielded anything, or no parameters were given.
-        _log.warning(f"No connections match the parameters innovation: {innovation}, input: {input_node},"
+        # No parameters were given, or the parameters were somehow faulty.
+        LOG.warning(f"No connections match the parameters innovation: {innovation}, input: {input_node},"
                      f" output: {output_node}")
         raise ValueError(f"No connections match the parameters innovation: {innovation}, input: {input_node},"
                          f" output: {output_node}")
 
-    def mutate_add_connection(self, in_node: int, out_node: int, weight: float, innovation: int) -> Connection:
+    def get_size(self) -> int:
+        """Get the total number of all connection genes in this genome."""
+        return len(self.connections)
+
+    def mutate_add_connection(self, in_node: int, out_node: int, innovation: int, weight: float) -> Connection:
         """Add a new connection linking two previously unconnected nodes."""
         # Check if the new connection already exists
-        try:
-            connex = self.get_connection(input_node=in_node, output_node=out_node)
-        except ValueError:
-            connex = None
+        connex = self.get_connection(input_node=in_node, output_node=out_node)
         if connex is not None:
             raise ValueError(f"A connection already exists between in: {in_node}, out: {out_node}")
 
-        mutated_connection = Connection(in_node, out_node, weight, innovation)
+        mutated_connection = Connection(in_node, out_node, innovation, weight)
 
         # TODO: Check if connection matches another genome's mutation, do not update innovation number if so.
         self.connections.append(mutated_connection)
-        _log.debug(f"Created mutated connection <in:{in_node}, out:{out_node}, weight:{weight}, inno:{innovation}>.")
+        LOG.debug(f"Created mutated connection <in:{in_node}, out:{out_node}, weight:{weight}, inno:{innovation}>.")
 
         return mutated_connection
 
@@ -90,10 +115,7 @@ class Genome:
         :return: A tuple containing the new node and connections.
         """
         # Ensure the global innovation number is not already in use for some reason.
-        try:
-            connex = self.get_connection(innovation=global_innovation)
-        except ValueError:
-            connex = None
+        connex = self.get_connection(innovation=global_innovation)
         if connex is not None:
             raise ValueError(f"The global innovation number {global_innovation} is already in use!")
 
@@ -108,14 +130,14 @@ class Genome:
 
         # Disable the old connection and replace it with two new ones.
         old_connection.enabled = False
-        connection_to_node = Connection(old_input, new_node_id, 1.0, global_innovation)
-        connection_from_node = Connection(new_node_id, old_output, old_connection.weight, global_innovation + 1)
+        connection_to_node = Connection(old_input, new_node_id, global_innovation, 1.0)
+        connection_from_node = Connection(new_node_id, old_output, global_innovation + 1, old_connection.weight)
 
         # TODO: Check if new connections match another genome's mutations, do not update innovation if they do.
 
         self.connections.append(connection_to_node)
         self.connections.append(connection_from_node)
-        _log.debug(f"Created mutated node with id {new_node_id} between nodes {old_input} and {old_output}.")
+        LOG.debug(f"Created mutated node with id {new_node_id} between nodes {old_input} and {old_output}.")
 
         return mutated_node, connection_to_node, connection_from_node
 
@@ -125,8 +147,50 @@ class Generation:
 
     def __init__(self):
         self.global_innovation = 1
-        self.mutated_connections = []
-        self.genomes = []
+        self.mutated_connections: List[Connection] = []
+        self.genomes: List[Genome] = []
+
+    def breed(self, first_parent: Genome, second_parent: Genome) -> Genome:
+        """Combine two genomes to produce offspring inheriting their combined features.
+
+        Disjoint or excess genes are inherited from the fitter parent.
+
+        :param first_parent: The first parent genome.
+        :param second_parent: The second parent genome.
+        :return: The offspring genome.
+        """
+        offspring = Genome()
+        # Find the parent with higher fitness to inherit genes from.
+        fit_parent = get_fittest((first_parent, second_parent))
+        bad_parent = first_parent if fit_parent is second_parent else second_parent
+
+        # Inputs and outputs are always identical.
+        offspring.input_nodes = first_parent.input_nodes
+        offspring.output_nodes = first_parent.output_nodes
+        # Hidden nodes are always inherited from the fitter parent.
+        offspring.hidden_nodes = fit_parent.hidden_nodes
+
+        # Connection genes are inherited randomly if they match, or from the fitter parent if they do not.
+        for fit_connection in fit_parent.connections:
+            bad_connection = bad_parent.get_connection(innovation=fit_connection.innovation)
+
+            if bad_connection is not None:
+                # Gene matches, inherit randomly.
+                random_gene = RANDOM.choice((fit_connection, bad_connection))
+                inherited_gene = random_gene.copy()
+                # If the gene is disabled in either parent, there is a chance to re-enable it.
+                if fit_connection.enabled is False or bad_connection.enabled is False:
+                    inherited_gene.enabled = DISABLED_GENE_REENABLE_CHANCE < RANDOM.random()
+            else:
+                # Gene is disjointed or excess, inherit directly.
+                inherited_gene = fit_connection.copy()
+                # If the gene is disabled, there is a chance to re-enable it.
+                if fit_connection.enabled is False:
+                    inherited_gene.enabled = DISABLED_GENE_REENABLE_CHANCE < RANDOM.random()
+
+            offspring.connections.append(inherited_gene)
+
+        return offspring
 
     def check_for_duplicate_innovation(self, input_node: int, output_node: int) -> Union[int, None]:
         """Check whether a connection between two nodes has already been created by any genome in the generation.
@@ -141,3 +205,91 @@ class Generation:
 
         return None
 
+
+def get_compatibility_distance(first_genome: Genome, second_genome: Genome) -> float:
+    """Calculate the compatibility distance between two genomes (network structures).
+
+    The distance increases with excess and disjoint genes, as well as weight differences between matching genes.
+
+    :param first_genome: The first genome.
+    :param second_genome: The second genome.
+    :return: A floating point value representing the compatibility distance.
+    """
+    # N is the number of genes of the larger genome.
+    if first_genome.get_size() > second_genome.get_size():
+        n_genes = first_genome.get_size()
+    else:
+        n_genes = second_genome.get_size()
+
+    # Count the total number of excess and disjoint genes. Prevent duplicates by keeping track of innovation numbers.
+    # This feels like a dumb way to do it. Can definitely be optimised.
+    processed_genes = []
+    excess, disjoint, weight = _get_gene_differences(first_genome, second_genome, processed_genes)
+    excess2, disjoint2, weight2 = _get_gene_differences(second_genome, first_genome, processed_genes)
+
+    excess += excess2
+    disjoint += disjoint2
+    weight += weight2
+
+    excess_distance = (EXCESS_GENES_COEFFICIENT * excess) / n_genes
+    disjoint_distance = (DISJOINT_GENES_COEFFICIENT * disjoint) / n_genes
+    weight_distance = WEIGHT_DIFFERENCE_COEFFICIENT * weight
+
+    return excess_distance + disjoint_distance + weight_distance
+
+
+def _get_gene_differences(first_genome: Genome, second_genome: Genome, processed_genes: List) -> Tuple[int, int, float]:
+    """Get the number of excess and disjoint genes along with total weight difference for two genomes.
+
+    Intended to be called twice, once for each genome in initial position. Directly edits the given list.
+    """
+    excess, disjoint, weight = 0, 0, 0.0
+    second_genome_last_innovation = second_genome.connections[-1].innovation
+
+    for first_gene in first_genome.connections:
+        if first_gene.innovation in processed_genes:
+            continue  # Duplicate, skip.
+
+        processed_genes.append(first_gene.innovation)
+        second_gene = second_genome.get_connection(innovation=first_gene.innovation)
+
+        if second_gene is None:
+            if first_gene.innovation > second_genome_last_innovation:
+                # Gene is excess
+                excess += 1
+            else:
+                # Gene is disjoint
+                disjoint += 1
+        else:
+            # Genes are matching, calculate weight difference
+            weight += math.fabs(first_gene.weight - second_gene.weight)
+
+    return excess, disjoint, weight
+
+
+def get_fittest(genomes: Sequence[Genome]) -> Genome:
+    """Find the fittest genome in a sequence of genomes.
+
+    Will choose a random one if two genomes have exactly the same fitness.
+
+    :param genomes: A sequence of genomes.
+    :return: The fittest genome in the input sequence.
+    """
+    fittest = None
+    for candidate in genomes:
+        # Special handling for the first candidate in the sequence, since there's nothing to compare it to yet.
+        if fittest is None:
+            fittest = candidate
+            continue
+        if candidate.fitness > fittest.fitness:
+            fittest = candidate
+        elif candidate.fitness == fittest.fitness:
+            # If fitness is equal, take a random one.
+            fittest = RANDOM.choice((candidate, fittest))
+
+    return fittest
+
+
+def sigmoid(x: float) -> float:
+    """Get Stanley & Miikkulainen's modified sigmoid of an input value."""
+    return 1 / (1 + math.exp(-4.9 * x))
--- tests/models/test_neatevolve.py
+++ tests/models/test_neatevolve.py
@@ -25,16 +25,62 @@ class TestGenetics:
         global_innovation = 1
         for in_node in test_genome.input_nodes:
             for out_node in test_genome.hidden_nodes:
-                connex = neat.Connection(in_node.id, out_node.id, 0.5, global_innovation)
+                connex = neat.Connection(in_node.id, out_node.id, global_innovation)
                 test_genome.connections.append(connex)
                 global_innovation += 1
         for in_node in test_genome.hidden_nodes:
             for out_node in test_genome.output_nodes:
-                connex = neat.Connection(in_node.id, out_node.id, 0.5, global_innovation)
+                connex = neat.Connection(in_node.id, out_node.id, global_innovation)
                 test_genome.connections.append(connex)
                 global_innovation += 1
 
         return test_genome
+
+    @pytest.fixture
+    def parent_genomes(self):
+        # Set up according to Figure 4 in Stanley & Miikkulainen.
+        first_parent = neat.Genome()
+        first_parent.fitness = 5
+        for i in range(1, 4):
+            first_parent.input_nodes.append(neat.Node(i))
+        first_parent.output_nodes.append(neat.Node(4))
+        first_parent.hidden_nodes.append(neat.Node(5))
+
+        con = []    # Connections to be added.
+        con.append(neat.Connection(1, 4, 1, 0.1))
+        c_two = neat.Connection(2, 4, 2, 0.6)
+        c_two.enabled = False
+        con.append(c_two)
+        con.append(neat.Connection(3, 4, 3, 0.7))
+        con.append(neat.Connection(2, 5, 4, 0.8))
+        con.append(neat.Connection(5, 4, 5, 0.2))
+        con.append(neat.Connection(1, 5, 8, 0.4))
+        first_parent.connections += con
+
+        # Deal with the second parent.
+        second_parent = neat.Genome()
+        second_parent.fitness = 2
+        for i in range(1, 4):
+            second_parent.input_nodes.append(neat.Node(i))
+        second_parent.output_nodes.append(neat.Node(4))
+        second_parent.hidden_nodes.append(neat.Node(5))
+        second_parent.hidden_nodes.append(neat.Node(6))
+
+        con = []
+        con.append(neat.Connection(1, 4, 1, 0.3))
+        con.append(c_two)
+        con.append(neat.Connection(3, 4, 3, 0.4))
+        con.append(neat.Connection(2, 5, 4, 0.8))
+        c_five = neat.Connection(5, 4, 5, 0.9)
+        c_five.enabled = False
+        con.append(c_five)
+        con.append(neat.Connection(5, 6, 6, 0.1))
+        con.append(neat.Connection(6, 4, 7, 0.6))
+        con.append(neat.Connection(3, 5, 9, 0.7))
+        con.append(neat.Connection(1, 6, 10, 1.0))
+        second_parent.connections += con
+
+        return first_parent, second_parent
 
     def test_get_connection_innovation(self, genome):
         connex = genome.get_connection(innovation=8)
@@ -43,10 +89,8 @@ class TestGenetics:
         assert connex.output_node == 9
 
     def test_get_connection_innovation_out_of_range(self, genome):
-        with pytest.raises(ValueError):
-            genome.get_connection(innovation=0)
-        with pytest.raises(ValueError):
-            genome.get_connection(innovation=100)
+        assert genome.get_connection(innovation=0) is None
+        assert genome.get_connection(innovation=100) is None
 
     def test_get_connection_nodes(self, genome):
         connex = genome.get_connection(input_node=1, output_node=8)
@@ -55,12 +99,9 @@ class TestGenetics:
         assert connex.output_node == 8
 
     def test_get_connection_nodes_no_connection(self, genome):
-        with pytest.raises(ValueError):
-            genome.get_connection(input_node=1, output_node=2)
-        with pytest.raises(ValueError):
-            genome.get_connection(input_node=5, output_node=6)
-        with pytest.raises(ValueError):
-            genome.get_connection(input_node=8, output_node=1)
+        assert genome.get_connection(input_node=1, output_node=2) is None
+        assert genome.get_connection(input_node=5, output_node=6) is None
+        assert genome.get_connection(input_node=8, output_node=1) is None
 
     def test_get_connection_bad_param(self, genome):
         with pytest.raises(ValueError):
@@ -71,7 +112,7 @@ class TestGenetics:
             genome.get_connection(output_node=8)
 
     def test_mutate_add_connection(self, genome):
-        connex = genome.mutate_add_connection(3, 7, 0.5, 100)
+        connex = genome.mutate_add_connection(3, 7, 100, 0.5)
         assert isinstance(connex, neat.Connection)
         assert connex.input_node == 3
         assert connex.output_node == 7
@@ -79,7 +120,7 @@ class TestGenetics:
 
     def test_mutate_add_connection_exists(self, genome):
         with pytest.raises(ValueError):
-            genome.mutate_add_connection(1, 8, 0.5, 100)
+            genome.mutate_add_connection(1, 8, 100, 0.5)
 
     def test_mutate_add_node(self, genome, caplog):
         # Try adding a new node between input node 1 and hidden node 1 (=id 8), which should be connection 1.
@@ -97,3 +138,13 @@ class TestGenetics:
     def test_mutate_add_note_already_used(self, genome):
         with pytest.raises(ValueError):
             genome.mutate_add_node(1, 11, 10)
+
+    def test_breed(self, parent_genomes):
+        generation = neat.Generation()
+        offspring = generation.breed(parent_genomes[0], parent_genomes[1])
+        assert len(offspring.hidden_nodes) == 1
+        assert len(offspring.connections) == 6
+
+    def test_get_compatibility_distance(self, parent_genomes):
+        # 2 excess, 3 disjoint, 1.2 weight, n_genes = 9
+        assert neat.get_compatibility_distance(parent_genomes[0], parent_genomes[1]) == 1.0355555555555556
